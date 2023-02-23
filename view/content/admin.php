865c7121be238bd423b1be915c295f1e8c2de8b2
JesusAIV <jesusalbertoisiquevasquez@gmail.com>
2023-02-22 19:44:45 -0500

    update

--- view/content/admin.php
+++ view/content/admin.php
@@ -1,1 +1,23 @@
-<h1>pagina admin</h1>
+<?php
+    $ajax = false;
+    require_once "./controller/gestionController.php";
+    $gestion = new gestionController();
+
+    // Lista de productos
+    $lista = $gestion->ListarProductos();
+?>
+
+<table class="tabla-produc-admin">
+    <tr>
+        <th>Nombre</th>
+        <th>Imagen</th>
+        <th>Usuario</th>
+    </tr>
+    <?php foreach ($lista as $result){ ?>
+    <tr>
+        <td class="center-flex"><div><?php echo $result -> nombre; ?></div></td>
+        <td class="center-flex"><div>S/.<?php echo $result -> precio; ?></div></td>
+        <td class="table-image"><img src="<?php echo SERVERURL.'view/'.$result -> imagen ?>" alt=""></td>
+    </tr>
+    <?php } ?>
+</table>
